d33e9c81e7b6fb8f19856c0e8259aa2f4eb66263
Fedor <fedor.radzhapov34505@gmail.com>
2025-10-08 17:52:44 +0300

    minor edit

--- answer.py
+++ answer.py
@@ -1,3 +1,6 @@
+import math
+
+
 CSI = '\x1b['
 RESET = f'{CSI}0m'
 ZERO = f'{CSI}0G'
@@ -10,49 +13,48 @@ END = f'{CSI}0m'
 
 #Флаг Польши
 for line in range(4):
-    if line<2:
-        print(f'{WHITE}{" "*18}{RESET}')
+    if line < 2:
+        print(f'{WHITE}{" " * 18}{RESET}')
     else:
         print(f'{RED}{" "*18}{RESET}')
 
 print('-'*70)
 
 #Узор
-length = 24
-hight = 5 #нечетное число
-for line in range(hight):
+LENGHT = 24
+HIGHT = 5 #нечетное число
+for line in range(HIGHT):
     if line%2==0:
-        print(f'{WHITE}{" "*length}{RESET}')
+        print(f'{WHITE}{" "*LENGHT}{RESET}')
     elif line%5==1:
-        print((f'{" "*8}{WHITE} {RESET} ')*(length//10))
+        print((f'{" " * 8}{WHITE} {RESET} ')*(LENGHT//10))
     else:
-        print((f'   {WHITE} {RESET}      ')*(length//10))
+        print((f'   {WHITE} {RESET}      ')*(LENGHT//10))
 
 print('-'*70)
 
 #График
-import math
-width = 40
-height = 12
-x_max = 16
-y_max = math.sqrt(x_max)
+WIDTH = 40
+HEIGHT = 12
+X_MAX = 16
+Y_MAX = math.sqrt(X_MAX)
 
-graph = [[" " for _ in range(width)] for _ in range(height)]
+graph = [[" " for _ in range(WIDTH)] for _ in range(HEIGHT)]
 
 y_axis = 2
-x_axis = height - 2
-for r in range(height):
+x_axis = HEIGHT - 2
+for r in range(HEIGHT):
     graph[r][y_axis] = "|"
-for c in range(y_axis, width):
+for c in range(y_axis, WIDTH):
     graph[x_axis][c] = "-"
 
 
-for i in range(width - y_axis - 1):
-    x = (i / (width - y_axis - 2)) * x_max
+for i in range(WIDTH - y_axis - 1):
+    x = (i / (WIDTH - y_axis - 2)) * X_MAX
     y = math.sqrt(x)
-    row = int(round((1 - y / y_max) * (x_axis - 1)))
+    row = int(round((1 - y / Y_MAX) * (x_axis - 1)))
     col = y_axis + 1 + i
-    if 0 <= row < height:
+    if 0 <= row < HEIGHT:
         graph[row][col] = ED
 
 for row in graph:
@@ -61,20 +63,23 @@ for row in graph:
 print('-'*70)
 
 #Процентное соотношение данных
-file = [abs(float(i.strip())) for i in open('sequence.txt')]
+seq_file = open('sequence.txt')
+file = [abs(float(i.strip())) for i in seq_file]
 perv = []
 vtor = []
 for i in range(len(file)):
-    if i<125:
+    if i < 125:
         perv.append(file[i])
     else:
         vtor.append(file[i])
 
-srp = sum(perv)/len(perv)
-srvt = sum(vtor)/len(vtor)
+srp = sum(perv) / len(perv)
+srvt = sum(vtor) / len(vtor)
 
-procperv = int((srp/(srp+srvt))*100)
-procvt = 100-procperv
+procperv = int((srp/(srp+srvt)) * 100)
+procvt = 100 - procperv
 
-print(f'Первые 125: {procperv}% {RED}{" "*(procperv//5)}{RESET}{"-"*(20-procperv//5)}|')
-print(f'Вторые 125: {procvt}% {GREEN}{" "*(procvt//5)}{RESET}{"-"*(20-procvt//5)}|')
+print(f'Первые 125: {procperv}% {RED}{" " * (procperv//5)}{RESET}{"-" * (20-procperv//5)}|')
+print(f'Вторые 125: {procvt}% {GREEN}{" " * (procvt//5)}{RESET}{"-" * (20-procvt//5)}|')
+
+seq_file.close()
